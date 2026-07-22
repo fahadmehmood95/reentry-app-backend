7c0427b175560d05db81f22a1d807eb8a188709b
fahadmehmood9507 <fahadmehmood@carpediemteam.org>
2026-07-22 22:26:49 +0500

    guards / decorators folders added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersModule } from '../users/users.module';
+import { MailModule } from '../mail/mail.module';
+import { ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: config.get<string>('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+
+    UsersModule,
+    MailModule,
+  ],
+
   controllers: [AuthController],
-  providers: [AuthService]
+
+  providers: [AuthService, JwtStrategy],
+
+  exports: [JwtModule, PassportModule],
 })
 export class AuthModule {}
